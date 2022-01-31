db1ef8632ee9d980e29a9e15567ca1b0d09da9e9
Aaron Jones <ronj2005@gmail.com>
2022-01-31 01:06:12 -0500

    update for heroku push

--- Develop/express.js
+++ Develop/express.js
@@ -14,37 +14,4 @@
 
 
 
-// addEventListener.js
-// allFunctionSyntax.js
-// allForLoopSyntax.js
-// allIfElse.js
-// allServers.js
-// appendElement.js
-// appendFile.js
-// appGet.js
-// appPosts.js
-// appListen.js
-// Arrays.js
-// async.js
-// classes.js
-// createElement.js
-// destructuring.js
-// express.js
-// fetchAPI.js
-// getFile.js
-// innerHTML.js
-// methods.js
-// node_npm.js
-// objects.js
-// parseJSON.js
-// reqANDres.js
-// requirePackages.js
-// routes.js
-// submit.js
-// testJS.js
-// timers.js
-// writeFile.js
-// wrileFolders.js
-// requireJSModules.js
-// addAttributes.js
-// templateLiterals.js
+
